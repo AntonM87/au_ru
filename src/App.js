243c857c134734ed4ff0e148d1eed28c645ee48a
Anton Martynenko <anton@MacBook-Air-Anton.local>
2022-01-05 01:45:11 +0500

    работает смена статуса

--- src/App.js
+++ src/App.js
@@ -1,48 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import Select from './Select/Select';
+import DropDownList from './DropDownList/DropDowList'
+import Card from './Card/Card';
 import './App.css';
 
 function App(props) {
 
   const { cities } = props
+  let [editMode, selectEditMode] = useState(false);
+  let [statusTooltip, setStatusTooltip] = useState('Прежде чем действовать, надо понять')
 
-  const filterPopulationArr = cities.filter(city => {
-    const population = city.population;
-    return population >= 5000 ? true : false;
-  })
-
-  const sortByMaxPopulationArr = filterPopulationArr.sort((prev, next) => {
-    return next.population - prev.population;
-  });
-
-  const maxPopulation = sortByMaxPopulationArr[0];
-
-  const sortByAlphabet = filterPopulationArr.sort((prev, next) => {
-    if (prev.city < next.city) {
-      return -1
-    } else if (prev.city > next.city) {
-      return 1;
-    } else {
-      return 0
-    }
-  });
-
-  const resArr = resConcatArr(maxPopulation, sortByAlphabet);
-
-  function resConcatArr(firstElem, array) {
-    let result = [];
-    result.push(firstElem);
-    array.forEach((val, i) => {
-      if (val === firstElem) return;
-      result.push(val);
-    })
-    return result;
-  };
+  function statusValidate(status) {
+    return status.length === 0 ? 'Здесь вы можете написать ваш статус' : statusTooltip;
+  }
 
   return (
-    <div className='container'>
-      <Select resArr={resArr} />
-    </div>
+    <div className='app'>
+      {/* <Card/> */}
+      {/* <DropDownList cities={cities} /> */}
+      <form>
+        <div className='header'>
+          <p>Здравствуйте,
+            <span>Человек №3596941</span>
+            {editMode ? <span onClick={() => {
+              selectEditMode(editMode = !editMode);
+            }} >Запомнить</span> :
+              <span onClick={() => {
+                selectEditMode(editMode = !editMode);
+              }} >Сменить статус</span>}
+          </p>
+        </div>
+        <div className='statusTooltip'>
+          {editMode ? <input onChange={(e) => {
+            setStatusTooltip(statusTooltip = e.target.value);
+            e.preventDefault();
+          }} defaultValue={statusTooltip}></input> : <span>{statusValidate(statusTooltip)}</span>}
+        </div>
+      </form >
+    </div >
   );
 }
 
---
+++ src/DropDownList/DropDowList.js
@@ -0,0 +1,47 @@
+
+import React, { useState, useEffect } from 'react';
+import Select from '../Select/Select'
+
+export default function DropDowList(props) {
+
+    const { cities } = props
+
+    const filterPopulationArr = cities.filter(city => {
+        const population = city.population;
+        return population >= 5000 ? true : false;
+    })
+
+    const sortByMaxPopulationArr = filterPopulationArr.sort((prev, next) => {
+        return next.population - prev.population;
+    });
+
+    const maxPopulation = sortByMaxPopulationArr[0];
+
+    const sortByAlphabet = filterPopulationArr.sort((prev, next) => {
+        if (prev.city < next.city) {
+            return -1
+        } else if (prev.city > next.city) {
+            return 1;
+        } else {
+            return 0
+        }
+    });
+
+    const resArr = resConcatArr(maxPopulation, sortByAlphabet);
+
+    function resConcatArr(firstElem, array) {
+        let result = [];
+        result.push(firstElem);
+        array.forEach((val, i) => {
+            if (val === firstElem) return;
+            result.push(val);
+        })
+        return result;
+    };
+
+    return (
+        <div className='container'>
+            <Select resArr={resArr} />
+        </div>
+    );
+}
